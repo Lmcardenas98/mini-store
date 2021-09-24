a38c182cce9d6ad6f2941e0dd2af981b85641d2f
Lmcardenas98 <lmcardenas.981106@gmail.com>
2021-09-24 10:52:21 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,48 @@
  **/
 
 console.log('Happy hacking :)')
+
+const baseUrl = 'https://platzi-avo.vercel.app';
+// const url = 'https://platzi-avo.vercel.app/api/avo';
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat('en-EN', {
+    style: 'currency',
+    currency: 'GBP',
+  }).format(price)
+  return newPrice;
+}
+
+//BASIC FETCH API
+// window
+//   .fetch(url)
+//   .then((response) => response.json())
+//   .then(response => {
+//     response.data.forEach(item => {
+//       console.log(item.name)
+//     })
+// })
+
+window
+  .fetch(`${baseUrl}/api/avo`)
+  .then((response) => response.json())
+  .then(response => {
+    const items = [];
+    response.data.forEach(item => {
+      const img = document.createElement('img');
+      img.src = `${baseUrl}${item.image}`
+
+      const name = document.createElement('h2');
+      name.textContent= item.name;
+
+      const price = document.createElement('div');
+      price.textContent = formatPrice(item.price);
+
+      const container = document.createElement('div');
+      container.append(img, name, price);
+
+      items.push(container);
+    });
+    appNode.append(...items)
+})
